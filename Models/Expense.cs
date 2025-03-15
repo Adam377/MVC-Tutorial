d43d1069d88be8ded911ae3083b01b8f3e3c80dd
Adam377 <adamhaigh185@yahoo.co.uk>
2025-03-15 10:34:45 +0000

    Added Expenses view

---
+++ Models/Expense.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Tutorial.Models
+{
+    public class Expense
+    {
+        #region Members
+        #endregion
+
+        #region Constructor
+        #endregion
+
+        #region Methods
+        public int Id { get; set; }
+        public decimal Value { get; set; }
+
+        [Required]
+        public string? Description { get; set; }
+        #endregion
+    }
+}
